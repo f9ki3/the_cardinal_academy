f93de04a641de87da55504e435664a257ff3ba75
Fyke Lleva <floterina@gmail.com>
2025-06-22 12:17:42 +0800

    updatyed

--- admin/navigation.php
+++ admin/navigation.php
@@ -7,7 +7,7 @@ $full_name = isset($user_info['full_name']) ? $user_info['full_name'] : 'Guest';
 $profile_image = isset($user_info['profile_image']) ? $user_info['profile_image'] : 'default.png';
 ?>
 
-<div id="nav_side" class="sidebar p-3 border-end sticky-top d-none d-md-block" style="min-height: 100vh; width: 250px;">
+<div id="nav_side" class="d-print-none sidebar p-3 border-end sticky-top d-none d-md-block" style="min-height: 100vh; width: 250px;">
     <div class="profile-pic mb-3 text-center">
         <img src="<?= htmlspecialchars($profile_image) ?>" alt="Profile" class="rounded-circle img-fluid" style="width: 80px; height: 80px; object-fit: cover;">
     </div>
