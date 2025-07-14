bf3ab03e1abe136f2e9e2bf93bf49acabfab2164
Fyke Lleva <floterina@gmail.com>
2025-07-14 16:53:29 +0800

    updated

--- teacher/navigation.php
+++ teacher/navigation.php
@@ -12,7 +12,7 @@ $profile_image = isset($user_info['profile_image']) ? $user_info['profile_image'
 
 <div id="nav_side" class="d-print-none sidebar p-3 border-end sticky-top d-none d-md-block" style="min-height: 100vh; width: 250px; overflow: hidden;">
     <div class="profile-pic mb-3 text-center">
-        <img src="<?= htmlspecialchars($profile_image) ?>" alt="Profile" class="rounded-circle img-fluid" style="width: 80px; height: 80px; object-fit: cover;">
+        <img src="<?= htmlspecialchars($profile_image) ?>" alt="Profile" class="border border-danger border-3 rounded-circle img-fluid" style="width: 80px; height: 80px; object-fit: cover;">
     </div>
     <h5 class="text-center fw-bolder text-dark mb-3"><?= htmlspecialchars($full_name) ?></h5>
     <hr class="text-dark">
