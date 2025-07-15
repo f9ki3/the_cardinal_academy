3033b5b3cf8c8bfe59b3a510d56f446752ced2b3
Fyke Lleva <floterina@gmail.com>
2025-07-16 02:50:57 +0800

    updated

--- teacher/navigation.php
+++ teacher/navigation.php
@@ -17,7 +17,7 @@ $profile_image = !empty($user['profile']) ? '../static/uploads/' . htmlspecialch
 
 
 <div id="nav_side" class="d-print-none sidebar p-3 border-end sticky-top d-none d-md-block" style="min-height: 100vh; width: 250px; overflow: hidden;">
-    <div class="profile-pic mb-3 text-center">
+    <div class="profile-pic mb-3 w-100  text-center">
         <img src="<?= htmlspecialchars($profile_image) ?>" alt="Profile" class="rounded-circle img-fluid" style="width: 80px; height: 80px; object-fit: cover;">
     </div>
 
