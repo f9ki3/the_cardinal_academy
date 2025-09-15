6bbbd75e934be7931b9393e68eafee950ed77e3a
Fyke Lleva <floterina@gmail.com>
2025-09-15 18:21:59 +0800

    database

--- db_connection.php
+++ db_connection.php
@@ -1,13 +1,13 @@
 <?php
-$host = 'localhost';        // usually localhost
-$db   = 'tca1';              // your actual DB name
-$user = 'root';             // your MySQL username
-$pass = '';                 // your MySQL password
-
 // $host = 'localhost';        // usually localhost
-// $db   = 'u429904263_tca';              // your actual DB name
-// $user = 'u429904263_tca';             // your MySQL username
-// $pass = 'UsKA?M[7';                 // your MySQL password
+// $db   = 'tca1';              // your actual DB name
+// $user = 'root';             // your MySQL username
+// $pass = '';                 // your MySQL password
+
+$host = 'localhost';        // usually localhost
+$db   = 'u429904263_tca';              // your actual DB name
+$user = 'u429904263_tca';             // your MySQL username
+$pass = 'UsKA?M[7';                 // your MySQL password
 
 // Create connection
 $conn = new mysqli($host, $user, $pass, $db);
